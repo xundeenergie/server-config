5a9aff7367ae755dfd832b51dc9f6c764b1649a6
Jakobus <jakobus.schuerz>
2019-01-28 20:07:56 +0100

    debug

--- functions.sh
+++ functions.sh
@@ -2,7 +2,9 @@ create_symlinks() {
 
     SCONFDIR="$1"
     DIR="$(basename ${SCONFDIR})"
+    pwd
     cd  "${SCONFDIR}"
+    pwd
     #Anlegen von Symlinks
     rm -rf ~/.vimrc ~/.vim ~/bashrc_add ~/.gitconfig
     ln -sf "${DIR}/vimrc" ~/.vimrc
