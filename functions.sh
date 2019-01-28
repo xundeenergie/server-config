9f7eed02cd0519fe650b5221e6ec056d302e85d0
Jakobus <jakobus.schuerz>
2019-01-28 20:19:02 +0100

    add credential-cache

--- functions.sh
+++ functions.sh
@@ -2,15 +2,18 @@ create_symlinks() {
 
     SCONFDIR="$1"
     DIR="$(basename ${SCONFDIR})"
-    pwd
     cd  "${SCONFDIR}"
-    pwd
+    git config credential.helper 'cache --timeout=300'
     #Anlegen von Symlinks
     rm -rf ~/.vimrc ~/.vim ~/bashrc_add ~/.gitconfig
     ln -sf "${DIR}/vimrc" ~/.vimrc
     ln -sf "${DIR}/vim" ~/.vim
     ln -sf "${DIR}/.gitconfig" ~/.gitconfig
     ln -sf "${DIR}/bashrc_add" ~/bashrc_add
+
+    git config core.hooksPath .githooks
+    find .git/hooks -type l -exec rm {} \; && find .githooks -type f -exec ln -sf ../../{} .git/hooks/ \;
+
     cd ~-
 
 }
