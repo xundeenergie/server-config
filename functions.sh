7c1e8b48dfcf5371045dd7206fc82519b6d14b14
Jakobus <jakobus.schuerz>
2019-01-28 20:05:06 +0100

    functions added, githooks added

---
+++ functions.sh
@@ -0,0 +1,15 @@
+create_symlinks() {
+
+    SCONFDIR="$1"
+    DIR="$(basename ${SCONFDIR})"
+    cd  "${SCONFDIR}"
+    #Anlegen von Symlinks
+    rm -rf ~/.vimrc ~/.vim ~/bashrc_add ~/.gitconfig
+    ln -sf "${DIR}/vimrc" ~/.vimrc
+    ln -sf "${DIR}/vim" ~/.vim
+    ln -sf "${DIR}/.gitconfig" ~/.gitconfig
+    ln -sf "${DIR}/bashrc_add" ~/bashrc_add
+    cd ~-
+
+}
+
