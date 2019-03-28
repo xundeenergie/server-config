9020ed295c470f0a3515a552a444306fc81654d4
Jakobus <jakobus.schuerz>
2019-03-28 14:37:51 +0100

    tmux added

--- functions.sh
+++ functions.sh
@@ -5,11 +5,13 @@ create_symlinks() {
     cd  "${SCONFDIR}"
     git config credential.helper 'cache --timeout=300'
     #Anlegen von Symlinks
-    rm -rf ~/.vimrc ~/.vim ~/bashrc_add ~/.gitconfig
+    rm -rf ~/.vimrc ~/.vim ~/bashrc_add ~/.gitconfig ~/.tmux.conf ~/.tmux
     ln -sf "${DIR}/vimrc" ~/.vimrc
     ln -sf "${DIR}/vim" ~/.vim
     ln -sf "${DIR}/.gitconfig" ~/.gitconfig
     ln -sf "${DIR}/bashrc_add" ~/bashrc_add
+    ln -sf "${DIR}/tmux.conf" ~/.tmux.conf
+    ln -sf "${DIR}/tmux" ~/.tmux
 
     git config core.hooksPath .githooks
     find .git/hooks -type l -exec rm {} \; && find .githooks -type f -exec ln -sf ../../{} .git/hooks/ \;
