6eb09039b3a6412d2e61875d364c6f6741d75ea4
Jakobus Schürz <jakobus.schuerz>
2019-12-02 09:40:56 +0100

    change url

--- bin/configserver.sh
+++ bin/configserver.sh
@@ -28,7 +28,9 @@ echo "Configure new Server (${SERVER}) for personal use"
 $CMD /bin/bash << EOF
     test -e "~/bashrc_add" && { echo "Server ${SERVER} configured"; exit 0; }
     rm -rf ~/bashrc_add
-    wget "https://git.schuerz.at/?p=public/server-config.git;a=blob_plain;f=bashrc_add;hb=HEAD" -O ~/bashrc_add
+    #wget "https://git.schuerz.at/?p=public/server-config.git;a=blob_plain;f=bashrc_add;hb=HEAD" -O ~/bashrc_add
+    git clone https://git.schuerz.at/public/server-config.git
+    ln -s server-config/bashrc_add bashrc_add
     echo "modify ~/.bashrc"
     if grep -q bashrc_add .bashrc ;then
         sed -i -e '/bashrc_add/d' .bashrc
