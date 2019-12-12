e92947e32eb2f0c8b0ad578fdb5c7cfee1d3558e
Jakobus Schürz <jakobus.schuerz>
2019-12-12 11:20:43 +0100

    change git to $SGIT

--- bin/git-checkout.sh
+++ bin/git-checkout.sh
@@ -11,6 +11,7 @@
 # notes         :                                                                               #
 #################################################################################################
 
+[ -z "${SGIT+x}" ] && SGIT=git
 SERVERCONFIG_LOGDIR="./logs"
 SERVERCONFIG_LOGFILE="${SERVERCONFIG_LOGDIR}/git.log"
 [ -d "${SERVERCONFIG_LOGDIR}" ] || mkdir -p "${SERVERCONFIG_LOGDIR}"
@@ -28,35 +29,35 @@ case $1 in
         PRE=""
         ;;
 esac
-git fetch origin -p  2>>"${SERVERCONFIG_LOGFILE}"|| { echo fetch failed; exit 1; }
+$SGIT fetch origin -p  2>>"${SERVERCONFIG_LOGFILE}"|| { echo fetch failed; exit 1; }
 
-if git diff-index --ignore-submodules --exit-code HEAD -- >> "${SERVERCONFIG_LOGFILE}" ; then
+if $SGIT diff-index --ignore-submodules --exit-code HEAD -- >> "${SERVERCONFIG_LOGFILE}" ; then
     cat << EOF >> "${SERVERCONFIG_LOGFILE}"
 Check for local changes:
     no changes in local repo
-    git checkout repo ${PRE}master
+    $SGIT checkout repo ${PRE}master
 EOF
     #echo "checkout origin/master as detached HEAD"
-    git checkout ${PRE}master 1>>"${SERVERCONFIG_LOGFILE}" 2>>"${SERVERCONFIG_LOGFILE}"|| exit 2
-    git merge FETCH_HEAD 1>>"${SERVERCONFIG_LOGFILE}" 2>>"${SERVERCONFIG_LOGFILE}"|| exit 3
+    $SGIT checkout ${PRE}master 1>>"${SERVERCONFIG_LOGFILE}" 2>>"${SERVERCONFIG_LOGFILE}"|| exit 2
+    $SGIT merge FETCH_HEAD 1>>"${SERVERCONFIG_LOGFILE}" 2>>"${SERVERCONFIG_LOGFILE}"|| exit 3
 else
     cat << EOF >> "${SERVERCONFIG_LOGFILE}"
 Check for local changes:
     Ich habe lokale Änderungen festgestellt
     um die Änderung zurückzusetzen bitte
 
-      git checkout \$FILENAME
+      $SGIT checkout \$FILENAME
 
     oder um alle lokalen Änderungen auf einmal zurückzusetzen:
 
-      git checkout .
+      $SGIT checkout .
 
     ausführen
 
     Die Änderungen sind:
 EOF
-    git diff-index HEAD --|awk '{print $5, $6}' >>  "${SERVERCONFIG_LOGFILE}"
-    git diff-index -p HEAD -- >> "${SERVERCONFIG_LOGFILE}"
+    $SGIT diff-index HEAD --|awk '{print $5, $6}' >>  "${SERVERCONFIG_LOGFILE}"
+    $SGIT diff-index -p HEAD -- >> "${SERVERCONFIG_LOGFILE}"
 
     echo "Lokale Änderungen festgestellt: Siehe Logfile ${SERVERCONFIG_LOGFILE}" >&2
 cat << EOF >> "${SERVERCONFIG_LOGFILE}"
@@ -74,16 +75,16 @@ cat << EOF >> "${SERVERCONFIG_LOGFILE}"
  
 EOF
 ## Update/init submodules
-#git submodule update --remote --merge 2>>"${SERVERCONFIG_LOGFILE}"|| { echo update submodules failed: continue ; }
-#git submodule init 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; exit 1; }
-#git submodule sync 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo sync submodules failed; exit 1; }
-#git submodule foreach "git branch -u origin/master master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo set-upstream submodules failed; exit 1; }
-#git submodule update --recursive --remote --merge 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; exit 1; }
+#$SGIT submodule update --remote --merge 2>>"${SERVERCONFIG_LOGFILE}"|| { echo update submodules failed: continue ; }
+#$SGIT submodule init 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; exit 1; }
+#$SGIT submodule sync 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo sync submodules failed; exit 1; }
+#$SGIT submodule foreach "$SGIT branch -u origin/master master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo set-upstream submodules failed; exit 1; }
+#$SGIT submodule update --recursive --remote --merge 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; exit 1; }
 
-git submodule init 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; }
-git submodule sync 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo sync submodules failed; }
-git submodule foreach "git branch -u origin/master master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo set-upstream submodules failed; }
-git submodule update --recursive --remote --merge 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; }
+$SGIT submodule init 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; }
+$SGIT submodule sync 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo sync submodules failed; }
+$SGIT submodule foreach "$SGIT branch -u origin/master master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo set-upstream submodules failed; }
+$SGIT submodule update --recursive --remote --merge 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed; }
 
 cat << EOF >> "${SERVERCONFIG_LOGFILE}"
 
