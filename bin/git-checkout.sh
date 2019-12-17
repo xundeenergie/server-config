6e71fcf063503330e0a42e51a18c655f9c8a049d
Jakobus Schürz <jakobus.schuerz>
2019-12-17 08:57:40 +0100

    add checkout master to git-checkout for submodules

--- bin/git-checkout.sh
+++ bin/git-checkout.sh
@@ -83,6 +83,7 @@ EOF
 
 $SGIT submodule init 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed on ini; }
 $SGIT submodule sync 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo sync submodules failed on sync; }
+$SGIT submodule foreach "git checkout master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo checkout master submodules failed on set upstream; }
 $SGIT submodule foreach "git branch -u origin/master master"  1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo set-upstream submodules failed on set upstream; }
 $SGIT submodule update --recursive --remote --merge 1>>"${SERVERCONFIG_LOGFILE}" 2>&1|| { echo update submodules failed on update; }
 
