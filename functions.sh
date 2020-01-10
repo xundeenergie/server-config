64e34ab15c1b0bd726a8f8ff435f772c9dcd0600
Jakobus Schürz <jakobus.schuerz>
2020-01-10 16:24:06 +0100

    add rcfile

--- functions.sh
+++ functions.sh
@@ -278,7 +278,7 @@ EOF
            ssh -T -o VisualHostKey=no $@ "cat > ${REMOTETMPVIMCONFIG}" < "${SERVERCONFIG_BASE}/vimrc"
            RCMD="
            trap \"rm -f ${REMOTETMPBASHCONFIG} ${REMOTETMPVIMCONFIG}\" EXIT " ;
-           ssh -t $@ "$RCMD; SSHS=true bash -c \"function bash () { /bin/bash; } ; export -f bash; exec bash --rcfile ${REMOTETMPBASHCONFIG}\""
+           ssh -t $@ "$RCMD; SSHS=true bash -c \"function bash () { /bin/bash --rcfile ${REMOTETMPBASHCONFIG} -i ; } ; export -f bash; exec bash --rcfile ${REMOTETMPBASHCONFIG}\""
            rm "${TMPBASHCONFIG}"
         else
            echo "${TMPBASHCONFIG} does not exist. Use »ssh $@«"
